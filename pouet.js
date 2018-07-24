ad943798939d743669de487325d3f36248a86613
pandrr <tom@drastic.net>
2018-07-24 19:12:27 +0200

    initial version

---
+++ pouet.js
@@ -0,0 +1,57 @@
+console.log("hack the planet");
+
+
+function getDemoZooData(id)
+{
+	fetch('//demozoo.org/api/v1/productions/'+id).then(function(response)
+	{
+		response.json().then(function(data)
+		{
+	        console.log(data);
+
+			if(data.screenshots && data.screenshots.length>0)
+			{
+				var el=document.querySelector("#screenshot img");
+				var screenIndex=0;
+				
+				el.src=data.screenshots[0].original_url;
+				el.onload=function()
+				{
+					el.classList.add('fixed');
+				}
+
+				el.onclick=function()
+				{
+					
+					console.log('num screens',data.screenshots.length);
+					screenIndex=(++screenIndex)%data.screenshots.length;
+
+					console.log(screenIndex);
+
+					el.src=data.screenshots[screenIndex].original_url;
+					
+				}
+
+			}
+		});
+	});	
+}
+
+
+
+var elDemoZooId=document.querySelector('#demozooID a');
+if(elDemoZooId)
+{
+	console.log(elDemoZooId);
+	console.log(elDemoZooId.href);
+
+	var parts=elDemoZooId.href.split('/');
+
+	console.log(parts);
+	getDemoZooData(parts[parts.length-2]);
+}
+else
+{
+	console.log('gelitten');
+}
+
